fc879581a6143f2aacf1803f4ebea99526c39944
brown46 <rlawlsxo623@naver.com>
2023-03-14 19:20:13 +0900

    테이블 수정

--- SQL쿼리문/dev01_dev01.sql
+++ SQL쿼리문/dev01_dev01.sql
@@ -1,38 +1,40 @@
 --<<사용자 테이블>>--
 --이용자 테이블 삭제
-drop table usersTable;
+drop table USERS;
 
 --이용자 테이블 생성
-create table usersTable(
-            usernum number NOT NULL
-          , userid VARCHAR2(20 char)
-          , username VARCHAR2(20 char)
-          , usernickname VARCHAR2(15 char) unique
-          , email VARCHAR2(100)
-          , password VARCHAR2(30)
-          , create_at DATE DEFAULT SYSDATE
-          , address VARCHAR2(1000)
-          , CONSTRAINT PK_USERTABLE PRIMARY KEY (userid)
+create table USERS(
+            userid VARCHAR2(20 char) NOT NULL
+          , email VARCHAR2(100) NOT NULL
+          , password VARCHAR2(30) NOT NULL
+          , nickname VARCHAR2(15 char) unique NOT NULL
+          , profile VARCHAR2(1000) NOT NULL
+          , profileurl VARCHAR2(300 char) NOT NULL
+          , CREATEAT TIMESTAMP DEFAULT SYSDATE NOT NULL
+          , UPDATEAT TIMESTAMP DEFAULT SYSDATE
+          , ISDELETED VARCHAR2(1 char) DEFAULT 'N'
+          , ISOPERATOR VARCHAR2(1 char) DEFAULT 'M'
+          , CONSTRAINT PK_USERS PRIMARY KEY (userid)
 );
 --이용자 테이블 조회
-select * from usersTable;
+select * from USERS;
 
 --이용자 추가(회원가입)
-select NVL(max(userid), 0)+1 from USERSTABLE;
-insert into USERSTABLE values((select NVL(max(usernum), 0)+1 from USERSTABLE)
+select NVL(max(userid), 0)+1 from USERS;
+insert into USERS values((select NVL(max(usernum), 0)+1 from USERS)
         , 'admin', '관리자', '모두의레시피관리자', 'admin@example.com', 'admin', default, '주소입력란');
 
-insert into USERSTABLE values((select NVL(max(usernum), 0)+1 from USERSTABLE)
+insert into USERS values((select NVL(max(usernum), 0)+1 from USERS)
         , '&userid', '&name', '&nickname', '&email', '&password', default, '&address');
 
 --회원탈퇴
-delete from USERSTABLE where userid='user1';
+delete from USERS where userid='user1';
 
 --회원정보 수정
-update USERSTABLE set username='사용자11', USERNICKNAME='이용자11', EMAIL='user11@example.com'
+update USERS set username='사용자11', USERNICKNAME='이용자11', EMAIL='user11@example.com'
                     , password = 'user11', address='서울시 강남구 학동' where userid='user1';
 
-update USERSTABLE set username='&name', USERNICKNAME='&nickname', EMAIL='&email'
+update USERS set username='&name', USERNICKNAME='&nickname', EMAIL='&email'
                     , password = '&password', address='&address' where userid='&userid';
 
 
@@ -42,7 +44,7 @@ update USERSTABLE set username='&name', USERNICKNAME='&nickname', EMAIL='&email'
 
 --<<게시판 게시글>>--
 --게시글 테이블
-CREATE TABLE POSTTABLE(
+CREATE TABLE POST(
        POSTID NUMBER PRIMARY KEY NOT NULL
      , USERID VARCHAR2(20 char) NOT NULL                        
      , USERNAME VARCHAR2(20 char) NOT NULL			        
@@ -50,8 +52,8 @@ CREATE TABLE POSTTABLE(
      , CONTENT CLOB NOT NULL
      , CREATEDATE TIMESTAMP DEFAULT(systimestamp) NOT NULL
      , UPDATEDATE TIMESTAMP DEFAULT(systimestamp) NOT NULL
-     , ISDELETED VARCHAR(10 char) NOT NULL
-     , CONSTRAINT FK_USERID_POST FOREIGN KEY(USERID) REFERENCES usersTable(userid)
+     , ISDELETED VARCHAR2(1 char) NOT NULL
+     , CONSTRAINT FK_USERID_POST FOREIGN KEY(USERID) REFERENCES USERS(userid)
 );
 
 --게시글 리스트 조회
@@ -59,24 +61,25 @@ CREATE TABLE POSTTABLE(
 --게시글 작성
 
 --게시글 삭제
-
+DROP TABLE POST;
 --게시글 수정
 
 --재료 테이블
 CREATE TABLE INGREDIENT(
      INGREDIENT VARCHAR2(30 char) NOT NULL
-    , POSTID NUMBER
-    , AMOUNT VARCHAR2(30 char)
-    , CONSTRAINT FK_PID_ING FOREIGN KEY(POSTID) REFERENCES POSTTABLE(POSTID)
+    , POSTID NUMBER NOT NULL
+    , AMOUNT VARCHAR2(30 char) NOT NULL
+    , CONSTRAINT FK_PID_ING FOREIGN KEY(POSTID) REFERENCES POST(POSTID)
     , CONSTRAINT PK_ING PRIMARY KEY(POSTID, INGREDIENT)
 );
 
 
 --이미지 테이블(수정필요)
-CREATE TABLE IMGTABLE(
-    IMGNAME VARCHAR2(50 char) NOT NULL
+CREATE TABLE POSTIMG(
+      IMGURL VARCHAR2(300 CHAR) NOT NULL
     , POSTID NUMBER NOT NULL
-    , CONSTRAINT FK_PID_IMG FOREIGN KEY(POSTID) REFERENCES POSTTABLE(POSTID)
+    , CONSTRAINT FK_PID_POSTIMG FOREIGN KEY(POSTID) REFERENCES POST(POSTID)
+    , CONSTRAINT PK_POSTIMG PRIMARY KEY(POSTID, IMGURL)
 );
 
 
@@ -84,38 +87,38 @@ CREATE TABLE IMGTABLE(
 CREATE TABLE HASHTAG(
     HASHTAG VARCHAR2(30 char) NOT NULL
     , POSTID NUMBER NOT NULL
-    , CONSTRAINT FK_PID_HTG FOREIGN KEY(POSTID) REFERENCES POSTTABLE(POSTID)
+    , CONSTRAINT FK_PID_HTG FOREIGN KEY(POSTID) REFERENCES POST(POSTID)
     , CONSTRAINT PK_HTG PRIMARY KEY(HASHTAG, POSTID)
 );
 
 --<<좋아요 테이블>>--
-drop table likeTable;
+drop table POSTLIKE;
 
-create table likeTable(
+create table POSTLIKE(
         LIKEID Number 
       , POSTID Number 
       , USERID VARCHAR2(20 char)
-      , CONSTRAINT FK_POSTID FOREIGN KEY(POSTID) REFERENCES POSTTABLE(POSTID)
+      , CONSTRAINT FK_POSTID FOREIGN KEY(POSTID) REFERENCES POST(POSTID)
       , CONSTRAINT FK_USERID FOREIGN KEY(USERID) REFERENCES usersTable(userid)
-      , CONSTRAINT PK_LIKETABLE PRIMARY KEY(POSTID, USERID)
+      , CONSTRAINT PK_POSTLIKE PRIMARY KEY(POSTID, USERID)
 );
 
-select * from likeTable;
+select * from POSTLIKE;
 
 
 --<<댓글 테이블>>--
-create table commentTable(
+create table COMMENT(
         CMTID number not null primary key
       , USERID VARCHAR2(20 char) not null
       , POSTID number not null
       , COMMENTCONTENT VARCHAR2(4000) not null
       , create_at TIMESTAMP default sysdate not null
       , updated_at TIMESTAMP
-      , constraint FK_userid_commentTable Foreign Key (userid) references usersTable (userid)
-      , constraint FK_postid_commentTable Foreign Key (postid) references postTable (postid)
+      , constraint FK_userid_COMMENT Foreign Key (userid) references usersTable (userid)
+      , constraint FK_postid_COMMENT Foreign Key (postid) references POST (postid)
 );
 --댓글 조회
-select * from commentTable;
+select * from COMMENT;
 
 --댓글 작성
 
