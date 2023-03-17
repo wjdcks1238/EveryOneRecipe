8e975fa2134f1b3e6b4db829f395557801df9745
wjdcks1238 <wjdcks1238@naver.com>
2023-03-17 14:10:10 +0900

    강사님이 확인해주실 예정입니다.
    파일 업로드 관련 이슈

--- src/main/java/com/kh/teamproject/post/ck/CkeditorImageFileUploadController.java
+++ src/main/java/com/kh/teamproject/post/ck/CkeditorImageFileUploadController.java
@@ -2,14 +2,20 @@ package com.kh.teamproject.post.ck;
 
 import java.io.File;
 import java.io.FileOutputStream;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,64 +25,72 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
+import com.cloudinary.Cloudinary;
+import com.cloudinary.utils.ObjectUtils;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.JsonObject;
 
+@PropertySource("classpath:apiKeys.properties")
 @Controller
 public class CkeditorImageFileUploadController {
-	@GetMapping("/abc")
-	public String abc() {
-		System.out.println("abc");
-		
-		return "index";
-	}
+	@Value("${cloudinary.name}") String apiName;
+	@Value("${cloudinary.key}") String apiKey;
+	@Value("${cloudinary.secret}") String apiSecret;
+	@Value("${cloudinary.url}") String apiUrl;
+	
 	@PostMapping("/upload.do")
 	@ResponseBody
 	public String fileUpload(
-//				  HttpServletRequest req
-//				, HttpServletResponse resp
-//				, MultipartHttpServletRequest multiFile
+				  HttpServletRequest req
+				, HttpServletResponse resp
+				, MultipartHttpServletRequest multiFile
 			) throws Exception {
 		System.out.println("out");
+		Gson gson = new Gson();
+		Gson gsonBuilder = new GsonBuilder().create();
 		JsonObject json = new JsonObject();
+		String jsonString = null;
 		PrintWriter printWriter = null;
+		InputStream in = null;
 		OutputStream out = null;
-//		MultipartFile file = multiFile.getFile("upload");
-//		if(file != null) {
-//			if(file.getSize() > 0 && StringUtils.isNotBlank(file.getName())) {
-//				if(file.getContentType().toLowerCase().startsWith("image/")) {
-//					try {
-//						String fileName = file.getName();
-//						byte[] bytes = file.getBytes();
-//						String uploadPath = req.getServletContext().getRealPath("/img");
-//						File uploadFile = new File(uploadPath);
-//						if(!uploadFile.exists()) {
-//							uploadFile.mkdirs();
-//						}
-//						fileName = UUID.randomUUID().toString();
-//						uploadPath = uploadPath + "/" + fileName;
-//						out = new FileOutputStream(new File(uploadPath));
-//						out.write(bytes);
-//						
-//						printWriter = resp.getWriter();
-//						resp.setContentType("text/html");
-//						String fileUrl = req.getContextPath() + "/img/" + fileName;
-//						
-//						json.addProperty("uploaded", 1);
-//						json.addProperty("fileName", fileName);
-//						json.addProperty("url", fileUrl);
-//						
-//						printWriter.println(json);
-//					} catch(Exception e) {
-//						
-//					} finally {
-//						if(out != null) {
-//							out.close();
-//						}
-//					}
-//				}
-//			}
-//		}
-//		
-		return json.toString();
+		MultipartFile file = multiFile.getFile("upload");
+		if(file != null) {
+			if(file.getSize() > 0 && StringUtils.isNotBlank(file.getName())) {
+				if(file.getContentType().toLowerCase().startsWith("image/")) {
+					try {
+						String originalFileName = file.getOriginalFilename();
+						File f = Files.createTempFile("temp",file.getOriginalFilename()).toFile();
+						file.transferTo(f);
+						
+						Map<String, String> config = new HashMap<String, String>();
+						config.put("cloud_name", apiName);
+						config.put("api_key", apiKey);
+						config.put("api_secret", apiSecret);
+						Cloudinary cloudinary = new Cloudinary(config);
+						
+						Map uploadResult = cloudinary.uploader().upload(f, ObjectUtils.emptyMap());
+						
+						System.out.println("출력 : " + uploadResult.get("url"));
+						System.out.println(uploadResult.get("url"));
+						
+						json.addProperty("uploaded", 1);
+						json.addProperty("fileName", originalFileName);
+						json.addProperty("url", );
+						jsonString = gson.toJson(json);
+						
+						printWriter.println(json);
+					} catch(Exception e) {
+						
+					} finally {
+						if(out != null) {
+							out.close();
+						}
+					}
+				}
+			}
+		}
+		
+		return jsonString;
 	}
 }
