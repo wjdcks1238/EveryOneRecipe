fed7f15c00d262c179267e6e6de17a82f7c7d682
wjdcks1238 <wjdcks1238@naver.com>
2023-03-17 14:56:51 +0900

    ckeditor 이미지 첨부 cloudinary api 사용 구현 완료
    -

--- src/main/java/com/kh/teamproject/post/ck/CkeditorImageFileUploadController.java
+++ src/main/java/com/kh/teamproject/post/ck/CkeditorImageFileUploadController.java
@@ -1,26 +1,17 @@
 package com.kh.teamproject.post.ck;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.util.HashMap;
 import java.util.Map;
-import java.util.UUID;
-
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
@@ -28,8 +19,6 @@ import org.springframework.web.multipart.MultipartHttpServletRequest;
 import com.cloudinary.Cloudinary;
 import com.cloudinary.utils.ObjectUtils;
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonObject;
 
 @PropertySource("classpath:apiKeys.properties")
 @Controller
@@ -42,22 +31,17 @@ public class CkeditorImageFileUploadController {
 	@PostMapping("/upload.do")
 	@ResponseBody
 	public String fileUpload(
-				  HttpServletRequest req
-				, HttpServletResponse resp
-				, MultipartHttpServletRequest multiFile
-			) throws Exception {
+				MultipartHttpServletRequest multiFile
+			) {
+		Map<String, String> map = null;
 		System.out.println("out");
 		Gson gson = new Gson();
-		Gson gsonBuilder = new GsonBuilder().create();
-		JsonObject json = new JsonObject();
-		String jsonString = null;
-		PrintWriter printWriter = null;
 		InputStream in = null;
 		OutputStream out = null;
 		MultipartFile file = multiFile.getFile("upload");
 		if(file != null) {
 			if(file.getSize() > 0 && StringUtils.isNotBlank(file.getName())) {
-				if(file.getContentType().toLowerCase().startsWith("image/")) {
+				if(file.getContentType().toLowerCase().startsWith("image/")) { //이미지 파일만 검색
 					try {
 						String originalFileName = file.getOriginalFilename();
 						File f = Files.createTempFile("temp",file.getOriginalFilename()).toFile();
@@ -71,26 +55,21 @@ public class CkeditorImageFileUploadController {
 						
 						Map uploadResult = cloudinary.uploader().upload(f, ObjectUtils.emptyMap());
 						
-						System.out.println("출력 : " + uploadResult.get("url"));
+						System.out.println("==============================================");
 						System.out.println(uploadResult.get("url"));
 						
-						json.addProperty("uploaded", 1);
-						json.addProperty("fileName", originalFileName);
-						json.addProperty("url", );
-						jsonString = gson.toJson(json);
+						map = new HashMap<String, String>();
 						
-						printWriter.println(json);
+						map.put("uploaded", "1");
+						map.put("fileName", originalFileName);
+						map.put("url", uploadResult.get("url").toString());
+						return gson.toJson(map);
 					} catch(Exception e) {
-						
-					} finally {
-						if(out != null) {
-							out.close();
-						}
-					}
+						e.printStackTrace();
+					} 
 				}
 			}
 		}
-		
-		return jsonString;
+		return "fail";
 	}
 }
