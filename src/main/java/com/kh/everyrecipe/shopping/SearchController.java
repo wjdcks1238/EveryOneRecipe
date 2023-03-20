75afbde42bc73c9186da6d58ad0b2d24eba07ae1
kyounghoon96 <doctorkh5@naver.com>
2023-03-20 22:07:50 +0900

    네이버쇼핑 API 수정

--- src/main/java/com/kh/everyrecipe/shopping/SearchController.java
+++ src/main/java/com/kh/everyrecipe/shopping/SearchController.java
@@ -1,6 +1,8 @@
 package com.kh.everyrecipe.shopping;
 
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -12,15 +14,23 @@ import java.util.List;
 @RequiredArgsConstructor
 @RestController	//JSON 형태로 객체 데이터 반환
 public class SearchController {
-    private final NaverShopping naverShop;
 
-    @RequestMapping(value="/shoptest", method = RequestMethod.GET)
+    private final NaverShopping naverShop;
+    
+    @GetMapping("/shoptest")
+    public ModelAndView goShopdata(ModelAndView mv) {
+    	mv.setViewName("shoptest");
+    	return mv;
+    }
+    
+    @RequestMapping(value="/shopdata", method = RequestMethod.GET)
     public ModelAndView getItems(ModelAndView mv
-    		, @RequestParam(value="query", defaultValue = "우유") String query) {  //임시로 우유값을 넣음
-    	String result = naverShop.search(query);  // query값 = 입력값
+    		, @RequestParam(value="query", required = false) String query) { //query값이 없다면 NULL입력
+    	String result = naverShop.search(query);  // query값 = 검색값
     	List<ItemVo> scitem = naverShop.JSONtoItems(result);
-    	mv.addObject("query", scitem);		// List형태의 값을 "query"에 담음
-    	mv.setViewName("/shoptest");    	
+    	
+    	mv.setViewName("shopdata");
+    	mv.addObject("query", scitem);		// List형태의 값을 "query"에 담음	
     	
     	return mv;
     }
