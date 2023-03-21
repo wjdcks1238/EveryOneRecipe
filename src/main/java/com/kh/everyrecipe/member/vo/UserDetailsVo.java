68048bf2b34ac8c458d7a8e39dc7487ad65f869c
SAEBONGCHON <saebongchon@gmail.com>
2023-03-21 11:51:24 +0900

    Security 수정 중

---
+++ src/main/java/com/kh/everyrecipe/member/vo/UserDetailsVo.java
@@ -0,0 +1,57 @@
+package com.kh.everyrecipe.member.vo;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public class UserDetailsVo implements UserDetails{
+
+	private String username;
+	private String password;
+	private List<GrantedAuthority> aythorities;
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
