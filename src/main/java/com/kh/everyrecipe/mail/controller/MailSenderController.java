3b3b36b68562332f124e5eef6e11fdef8b7d5640
wjdcks1238 <wjdcks1238@naver.com>
2023-03-24 10:07:51 +0900

    메일인증 API 구현
    이메일 인증번호 발송하실 때 사용하시면 될 듯 합니다.

--- src/main/java/com/kh/everyrecipe/mail/controller/MailSenderController.java
+++ src/main/java/com/kh/everyrecipe/mail/controller/MailSenderController.java
@@ -1,5 +1,7 @@
 package com.kh.everyrecipe.mail.controller;
 
+import java.util.Random;
+
 import javax.mail.internet.MimeMessage;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -24,20 +27,25 @@ public class MailSenderController {
 		return "mail/auth";
 	}
 
-    @ResponseBody
-	@PostMapping(value = "/auth", produces = "text/html; charset=UTF-8") 
-    public String sendAuth(@RequestBody (required = false)String email) { 
+	@PostMapping("/auth") 
+    public String sendAuth(@RequestParam("email")String email) { 
     //﻿(required = false) 매개변수의 null의 여부를 지정 -> false ==  null일 수도 있다.
     	MimeMessage message = javaMailSender.createMimeMessage();
+    	System.out.println("이메일주소 : " + email);
     	
+    	Random rd = new Random();
+    	
+    	int authCode = rd.nextInt(999999)+1;
+    	String msg = "인증번호를 입력해 주세요 : \n" + authCode;
         try {
 			MimeMessageHelper msgHelper = new MimeMessageHelper(message, true, "UTF-8");
 			//헬퍼를 이용해서 email 연결 정보를 입력하는게 더 편리함
 			//데이터의 양이 많다면 두번쨰 영역을 true를 하면된다 == multipart
 			//마지막은 인코딩 형식 == utf-8
 			
-			msgHelper.setSubject("인증메일제목"); //이메일의 제목
-			msgHelper.setText("인증번호"); //이메일의 내용
+			
+			msgHelper.setSubject("'모두의 레시피' 인증번호"); //이메일의 제목
+			msgHelper.setText(msg); //이메일의 내용
 			msgHelper.setTo(email); //jsp에서 받은 수신자 이메일 번호
 			
 			 
@@ -47,7 +55,7 @@ public class MailSenderController {
 			e.printStackTrace();
 		}
 
-        return "송신 완료"; //이상 없을 시 문자열 jsp에 리턴
+        return "redirect:auth"; //이상 없을 시 문자열 jsp에 리턴
      }
 
 
