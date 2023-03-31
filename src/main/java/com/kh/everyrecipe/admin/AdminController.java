6a77133cfb650c2c89b0a7d66683824216b117ee
SAEBONGCHON <saebongchon@gmail.com>
2023-03-31 22:36:18 +0900

    관리자페이지 수정중

--- src/main/java/com/kh/everyrecipe/admin/AdminController.java
+++ src/main/java/com/kh/everyrecipe/admin/AdminController.java
@@ -1,12 +1,26 @@
 package com.kh.everyrecipe.admin;
 
+import java.security.Principal;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.kh.everyrecipe.member.dao.MemberDao;
+import com.kh.everyrecipe.member.service.MemberService;
+import com.kh.everyrecipe.member.vo.MemberVo;
 
 @Controller
 public class AdminController {
-
+	
+	@Autowired 
+	private MemberService mService;
 	
 	@GetMapping("/admin")
 	public String adminTest() {
@@ -15,10 +29,23 @@ public class AdminController {
 	}
 	
 
+//	@GetMapping("/admin/members")
+//	public String members() {
+//		
+//		return "/admin/members";
+//	}
+	
+	//직원 관리 페이지
 	@GetMapping("/admin/members")
-	public String members() {
+	public ModelAndView members(ModelAndView mv, Principal principal) throws Exception {
+
+		List<MemberVo> list = mService.selectList();
 		
-		return "/admin/members";
+		mv.addObject("memberDto", list);
+		
+		mv.setViewName("admin/members");
+		
+		return mv;
 	}
 	
 	
