531bba578b9bc9537e3f6385fc7557fd325fb04f
kyounghoon96 <doctorkh5@naver.com>
2023-04-04 15:13:23 +0900

    검색어 순위 추가(임시)

---
+++ src/main/java/com/kh/everyrecipe/weekboard/vo/popularVo.java
@@ -0,0 +1,17 @@
+package com.kh.everyrecipe.weekboard.vo;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Component
+@Getter
+@Setter
+@ToString
+public class popularVo {
+	private String keword;
+	private int times;
+	
+}
