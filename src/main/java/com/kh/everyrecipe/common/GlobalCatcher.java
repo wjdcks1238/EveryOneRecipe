b0b8319bad93a043725e135361386fc2ce5eda79
brown46 <rlawlsxo623@naver.com>
2023-04-17 13:51:39 +0900

    alert 창 이후에 작성글 유지

--- src/main/java/com/kh/everyrecipe/common/GlobalCatcher.java
+++ src/main/java/com/kh/everyrecipe/common/GlobalCatcher.java
@@ -4,9 +4,20 @@ package com.kh.everyrecipe.common;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @ControllerAdvice("com.kh.everyrecipe")
+
+
 public class GlobalCatcher {
+	
+	
+	@ExceptionHandler(BadWordException.class)
+	public String badWordCatcher(Exception ex , Model m) {
+		//rttr.addFlashAttribute("ex",ex );
+		m.addAttribute(ex.getMessage());
+		return "post/posting";
+	}
 	@ExceptionHandler(Exception.class)
 	public String catcher(Exception ex , Model m) {
 		m.addAttribute("ex",ex );
