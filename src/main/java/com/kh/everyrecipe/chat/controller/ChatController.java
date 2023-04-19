21169238eff349efeb49477bc3f75da46db29b24
kyounghoon96 <doctorkh5@naver.com>
2023-04-19 14:51:54 +0900

    채팅 기능 작업중(id 선택해서 보내기)

--- src/main/java/com/kh/everyrecipe/chat/controller/ChatController.java
+++ src/main/java/com/kh/everyrecipe/chat/controller/ChatController.java
@@ -1,21 +1,28 @@
 package com.kh.everyrecipe.chat.controller;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.kh.everyrecipe.member.service.MemberService;
+import com.kh.everyrecipe.member.vo.MemberVo;
 
 
 @Controller
 public class ChatController {
-	
-	private static final Logger logger = LoggerFactory.getLogger(ChatController.class);
+	@Autowired
+	private MemberService Service;
 	
 	@GetMapping("/chat")
-	public String chatGet() {
-		logger.info("@ChatController, chatGet()");
-		
-		return "chat";
+	public ModelAndView chatGet(ModelAndView mv) throws Exception {
+		//사용자 리스트 불러오기
+		List<MemberVo> idlist = Service.selectList();
+		mv.addObject("idlist", idlist);
+		mv.setViewName("chat");
+		return mv;
 	}
 	
 }
